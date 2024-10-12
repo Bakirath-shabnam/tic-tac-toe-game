f1cbccd491a4def079d9f91f751e26020b55c64a
Bakirath-shabnam <151817728+Bakirath-shabnam@users.noreply.github.com>
2024-10-12 20:31:29 +0530

    script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let boxes = document.querySelectorAll(".box");
 let turn = "X";
 let isGameOver = false;
 
-boxes.forEach(e =>{
+boxes.forEach(e => {
     e.innerHTML = ""
-    e.addEventListener("click", ()=>{
-        if(!isGameOver && e.innerHTML === ""){
+    e.addEventListener("click", () => {
+        if (!isGameOver && e.innerHTML === "") {
             e.innerHTML = turn;
             cheakWin();
             cheakDraw();
@@ -15,49 +15,48 @@ boxes.forEach(e =>{
     })
 })
 
-function changeTurn(){
-    if(turn === "X"){
+function changeTurn() {
+    if (turn === "X") {
         turn = "O";
         document.querySelector(".bg").style.left = "85px";
-    }
-    else{
+    } else {
         turn = "X";
         document.querySelector(".bg").style.left = "0";
     }
 }
 
-function cheakWin(){
+function cheakWin() {
     let winConditions = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ]
-    for(let i = 0; i<winConditions.length; i++){
+    for (let i = 0; i < winConditions.length; i++) {
         let v0 = boxes[winConditions[i][0]].innerHTML;
         let v1 = boxes[winConditions[i][1]].innerHTML;
         let v2 = boxes[winConditions[i][2]].innerHTML;
 
-        if(v0 != "" && v0 === v1 && v0 === v2){
+        if (v0 != "" && v0 === v1 && v0 === v2) {
             isGameOver = true;
-            document.querySelector("#results").innerHTML = turn + " win";
+            document.querySelector("#results").innerHTML = turn + " wins!";
             document.querySelector("#play-again").style.display = "inline"
 
-            for(j = 0; j<3; j++){
-                boxes[winConditions[i][j]].style.backgroundColor = "#08D9D6"
-                boxes[winConditions[i][j]].style.color = "#000"
+            for (j = 0; j < 3; j++) {
+                boxes[winConditions[i][j]].style.backgroundColor = "#7b9a7a";
+                boxes[winConditions[i][j]].style.color = "#000";
             }
         }
     }
 }
 
-function cheakDraw(){
-    if(!isGameOver){
+function cheakDraw() {
+    if (!isGameOver) {
         let isDraw = true;
-        boxes.forEach(e =>{
-            if(e.innerHTML === "") isDraw = false;
+        boxes.forEach(e => {
+            if (e.innerHTML === "") isDraw = false;
         })
 
-        if(isDraw){
+        if (isDraw) {
             isGameOver = true;
             document.querySelector("#results").innerHTML = "Draw";
             document.querySelector("#play-again").style.display = "inline"
@@ -65,14 +64,14 @@ function cheakDraw(){
     }
 }
 
-document.querySelector("#play-again").addEventListener("click", ()=>{
+document.querySelector("#play-again").addEventListener("click", () => {
     isGameOver = false;
     turn = "X";
     document.querySelector(".bg").style.left = "0";
     document.querySelector("#results").innerHTML = "";
     document.querySelector("#play-again").style.display = "none";
 
-    boxes.forEach(e =>{
+    boxes.forEach(e => {
         e.innerHTML = "";
         e.style.removeProperty("background-color");
         e.style.color = "#fff"
